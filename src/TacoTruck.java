f7e45106c6ea9387fd3ccced07f415974d670f01
Burton86 <kyleb286@gmail.com>
2019-08-28 09:18:07 -0500

    update

---
+++ src/TacoTruck.java
@@ -0,0 +1,29 @@
+public class TacoTruck {
+    public static void main(String[] args){
+   System.out.println("Taco Truck" );
+        System.out.println("n\n\n\n");
+
+    displayTacoTruck();
+
+}
+    public static void displayTacoTruck () {
+
+        System.out.println("TacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruck");
+        System.out.println("TacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckt");
+        System.out.println("TacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckta");
+        System.out.println("TacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTrucktac");
+        System.out.println("TacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTaco");
+        System.out.println("TacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoT");
+        System.out.println("TacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruck");
+        System.out.println("TacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruck");
+        System.out.println("TacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruck");
+        System.out.println("TacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruck");
+        System.out.println("TacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruck");
+        System.out.println("TacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruck");
+        System.out.println("TacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruck");
+        System.out.println("TacoTruckTacoTruck                        tacotrucktacotruck");
+        System.out.println("TacoTruckTacoTruck                        tacotrucktacotruck");
+        System.out.println(" TacoTruckTruck                              tacotrucktruck");
+        System.out.println("   tacotruck                                  tacotruck");
+    }
+}
